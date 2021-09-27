a0383abb079c60df458f11fefb196e773d69bbe7
mkn025 <78483320+mkn025@users.noreply.github.com>
2021-09-27 12:20:29 +0200

    testing

---
+++ pygame /bevegelse.py
@@ -0,0 +1,44 @@
+import pygame
+import os 
+
+
+
+
+pygame.init()
+
+HIMMELBLA = (135, 206, 235)
+GRØNN = (124,252,0)
+SOL = (255,255,0)
+brun = (139,69,19)
+vindu = pygame.display.set_mode((300, 300))
+white =(255, 255, 255) 
+
+clock = pygame.time.Clock()
+
+
+
+
+x = 0
+y = 0
+dx = 15
+dy = 15
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    clock.tick(10)
+    vindu.fill(HIMMELBLA)
+    pygame.draw.circle(vindu, SOL, (x,y), 10, width=0)
+    x += dx
+    y += dy
+
+
+    print(x, y)
+    if x == 300 and y == 300:
+        x -= dx
+        y -= dy
+    pygame.draw.circle(vindu, SOL, (x,y), 10, width=0)
+    pygame.display.update()
+
