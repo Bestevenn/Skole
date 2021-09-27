feb90c24ab6b538017dd1573a0a69896dbbb6a65
mkn025 <78483320+mkn025@users.noreply.github.com>
2021-09-27 12:20:24 +0200

    testing

---
+++ pygame /første pygame.py
@@ -0,0 +1,32 @@
+import pygame 
+import time
+
+pygame.init()
+
+HIMMELBLA = (135, 206, 235)
+GRØNN = (124,252,0)
+SOL = (255,255,0)
+brun = (139,69,19)
+vindu = pygame.display.set_mode((400, 300))
+white =(255, 255, 255) 
+
+clock = pygame.time.Clock()
+
+
+
+
+x = 0
+y = 150
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    clock.tick(10)
+    vindu.fill(HIMMELBLA)
+    pygame.draw.circle(vindu, SOL, (x,y), 10, width=0)
+    x += 1
+    pygame.draw.rect(vindu, GRØNN, (0,200,400,400), width=0)
+    pygame.draw.polygon(vindu, brun, [(0,200),(50,100),(100,200)])
+    pygame.draw.polygon(vindu, brun, [(100,200),(150,100),(200,200)])
+    pygame.display.update()
+
