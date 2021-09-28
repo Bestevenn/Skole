4d17c1ce7ff89aa7b48e0fcc00063d9666956a8f
mkn025 <78483320+mkn025@users.noreply.github.com>
2021-09-28 17:11:11 +0200

    Sol = SUN

--- pygame /første pygame.py
+++ pygame /første pygame.py
@@ -5,7 +5,7 @@ pygame.init()
 
 HIMMELBLA = (135, 206, 235)
 GRØNN = (124,252,0)
-SOL = (255,255,0)
+SUN = (255,255,0)
 brun = (139,69,19)
 vindu = pygame.display.set_mode((400, 300))
 white =(255, 255, 255) 
@@ -23,7 +23,7 @@ while True:
             pygame.quit()
     clock.tick(10)
     vindu.fill(HIMMELBLA)
-    pygame.draw.circle(vindu, SOL, (x,y), 10, width=0)
+    pygame.draw.circle(vindu, SUN, (x,y), 10, width=0)
     x += 1
     pygame.draw.rect(vindu, GRØNN, (0,200,400,400), width=0)
     pygame.draw.polygon(vindu, brun, [(0,200),(50,100),(100,200)])
