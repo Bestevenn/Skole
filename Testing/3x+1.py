0144d4cf43b52d64e0229c84288826ab9c20a826
mkn025 <78483320+mkn025@users.noreply.github.com>
2021-10-06 21:58:24 +0200

    main

--- Testing/3x+1.py
+++ Testing/3x+1.py
@@ -7,24 +7,45 @@ import math
 
 
 
-n = float(2**9)
-tall = n
-z = 0
-print("")
-print("Tall:",n)
-print("")
-while n > 1:
-        if n%2 == 0:
-            n =  n/2
-            z += 1
-            print(f"Nr.{z}     {n} ")
-        elif n%2 == 1:
-            n = 3*n +1
-            z += 1
-            print(f"Nr.{z}     {n} ")
+#deffinerer en funkjson som vi kan kalle når som helst i progammret
+def algoritme(tall):
+    n = float(tall)
+    tall = n
+    z = 0
+    print("")
+    print("Tall:",n)
+    print("")
 
-print("")
-print("Det tok", z-1, "forsøk og komme til 0 med tallet",tall )
-print("")
+    while n > 1:
+            if n%2 == 0:
+                n =  n/2
+                z += 1
+                print(f"Nr.{z}     {n} ")
+            elif n%2 == 1:
+                n = 3*n +1
+                z += 1
+                print(f"Nr.{z}     {n} ")
+
+    print("")
+    print("Det tok", z-1, "forsøk og komme til 0 med tallet",tall )
+    print("")
+
+
+Fortsett = True
+
+while Fortsett:
+    print("")
+    brukertall = input("skriv et tall/eller skriv 'flere' for flere tall s for stopp : ")
+    print("")
+    if brukertall == "s":
+        Fortsett = False
+    elif brukertall == "flere":
+        start = int(input("start?: "))
+        stop = int(input("stop?: "))
+
+        for n in range(start,stop+1):
+            algoritme(n)
+    else:
+        algoritme(brukertall)
 
 
