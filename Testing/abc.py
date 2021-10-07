2499f9526cd913cc14c66666c5fa3bf601135679
mkn025 <78483320+mkn025@users.noreply.github.com>
2021-10-07 13:27:42 +0200

    Ferdig

--- Testing/abc.py
+++ Testing/abc.py
@@ -1,17 +1,15 @@
 from math import sqrt
-a,b,c = float(2),float(8),float(8)
-svar1, svar2 = (-b+sqrt(b**2-4*a*c)) / (2*a), (-b-sqrt(b**2-4*a*c)) / (2*a)
-
-if b**2-4*a*c > 0:
-    print("x1: ", svar1,"x2: ", svar2)
-elif b**2-4*a*c == 0:
-    try:
-        print("x1: ",svar1)
-    except:
-        print("x1: ",svar2)
-elif b**2-4*a*c < 0:
-    print("ingen svar")
+def svar_skjekk(a,b,c):
+    svar1, svar2 = (-b+sqrt(b**2-4*a*c)) / (2*a), (-b-sqrt(b**2-4*a*c)) / (2*a)
+    if b**2-4*a*c > 0:
+        print("x1: ", svar1,"x2: ", svar2)
+    elif b**2-4*a*c == 0:
+        try:
+            print("x1: ",svar1)
+        except:
+            print("x1: ",svar2)
+    elif b**2-4*a*c < 0:
+        print("ingen svar")
 
         
-
-
+svar_skjekk(2,8,8)
