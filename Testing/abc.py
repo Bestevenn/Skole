a2a9a8735541d5b7bd8caa52e969ca3de7c90478
mkn025 <78483320+mkn025@users.noreply.github.com>
2021-10-07 13:18:12 +0200

    main

--- Testing/abc.py
+++ Testing/abc.py
@@ -1,7 +1,17 @@
-import math
-a,b,c = float(2), float(12),float(-7)
-svar1, svar2 = (-b+math.sqrt(b**2-4*a*c)) / (2*a), (-b-math.sqrt(b**2-4*a*c)) / (2*a)
-print("x1: ", svar1,"x2: ", svar2)
+from math import sqrt
+a,b,c = float(2),float(8),float(8)
+svar1, svar2 = (-b+sqrt(b**2-4*a*c)) / (2*a), (-b-sqrt(b**2-4*a*c)) / (2*a)
 
+if b**2-4*a*c > 0:
+    print("x1: ", svar1,"x2: ", svar2)
+elif b**2-4*a*c == 0:
+    try:
+        print("x1: ",svar1)
+    except:
+        print("x1: ",svar2)
+elif b**2-4*a*c < 0:
+    print("ingen svar")
+
+        
 
 
