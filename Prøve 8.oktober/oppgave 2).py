00fd6a33c6aa6b93e53b21ce3e80fc1d08fe977e
mkn025 <78483320+mkn025@users.noreply.github.com>
2021-10-08 09:49:00 +0200

    prøve

---
+++ Prøve 8.oktober/oppgave 2).py
@@ -0,0 +1,22 @@
+#oppgave 2
+
+import random
+
+antall_kast = 3
+for n in range(antall_kast):
+    terning1 = random.randint(1,6)
+    print("ditt kast")
+    print(terning1)
+    t1 = 0
+    if terning1 == 6:
+        print("Du fikk seks. kast en gang til")
+        t1  = input("skriv kast for å kaste en gang til: ")
+    if t1 == "kast":
+        terning = random.randint(1,6)
+        print("du kan flytte", terning, "fremover")
+        exit()
+else:
+    print("du må vente til neste runde")      
+    
+ 
+    
