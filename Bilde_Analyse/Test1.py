55e54c7814a26ea43922e43c9bd86f4be55e0f42
mkn025 <martin.elde.knutsen@gmail.com>
2021-10-12 17:22:39 +0200

    main

--- Bilde_Analyse/Test1.py
+++ Bilde_Analyse/Test1.py
@@ -3,5 +3,5 @@ from PIL import Image
 import os
 img = Image.open(
     "Bilde_Analyse/Skjermbilde 2021-10-12 kl. 00.53.47.png")
-text = pytesseract.image_to_string(img)
+text = pytesseract.image_to_boxes(img)
 print(text)
