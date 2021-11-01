7352242a72affab7a474a231391ecea3d0b594d2
mkn025 <78483320+mkn025@users.noreply.github.com>
2021-11-01 18:13:19 +0100

    main

--- Testing/log.py
+++ Testing/log.py
@@ -1,15 +1,15 @@
 from math import log
 
+
 def logtest():
     for n in range(100+1):
-        a,b,c = n, n+1, n+2
+        a, b, c = n, n+1, n+2
         svar1 = log(a*b*c)
         svar2 = log(a) + log(b) + log(c)
         if svar1 == svar2:
             print("TRUE")
         else:
-            print("FALSE    ", svar1,svar2, "orginale tall = " ,a,b,c)
+            print("FALSE    ", svar1, svar2, "orginale tall = ", a, b, c)
 
 
 logtest()
-
--- pygame/Køddespill.py
+++ pygame/Køddespill.py
@@ -2,16 +2,19 @@ import pygame
 from random import randint
 pygame.init
 
-x_vin, y_vin = (1920),(1080)
+x_vin, y_vin = (800), (800)
 backgrunn = (30, 30, 30)
 farge_ball = (138, 180, 248)
-fps = 120 
+fps = 120
 vindu = pygame.display.set_mode((x_vin, y_vin))
 clock = pygame.time.Clock()
 
-x_kod_ball = 960
-y_kod_ball = 540
-radius = 50 
+x_kod_ball = 400
+y_kod_ball = 400
+x_kod_ball2 = 100
+y_kod_ball2 = 100
+radius = 50
+radius_ball2 = 50
 dx = 5
 dy = 5
 R = 2.5
@@ -20,7 +23,22 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
     vindu.fill(backgrunn)
-    pygame.draw.circle(vindu, farge_ball, (x_kod_ball, y_kod_ball), radius, width=0)
+    pygame.draw.circle(vindu, farge_ball, (x_kod_ball,
+                       y_kod_ball), radius, width=0)
+    pygame.draw.circle(vindu, farge_ball, (x_kod_ball2,
+                       y_kod_ball2), radius, width=0)
+    x_kod_ball2 += 1
+    y_kod_ball2 += 1
+    if x_kod_ball2 > x_vin + radius:
+        x_kod_ball2 -= dx
+    if x_kod_ball2 < x_vin - radius:
+        x_kod_ball2 += dx
+    if y_kod_ball2 > y_vin + radius:
+        y_kod_ball2 -= dy
+    if y_kod_ball2 < y_vin - radius:
+        y_kod_ball2 += dx
+    print(y_kod_ball2)
+    print(x_kod_ball2)
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_LEFT:
             x_kod_ball -= dx
@@ -30,21 +48,18 @@ while True:
             y_kod_ball -= dy
         if event.key == pygame.K_DOWN:
             y_kod_ball += dy
+        if event.key == pygame.K_ESCAPE:
+            pygame.quit()
         if x_kod_ball > x_vin + radius:
             x_kod_ball = 0 - radius
         if x_kod_ball < 0 - radius:
-            x_kod_ball = 1920 + radius
-        
+            x_kod_ball = 800 + radius
         if y_kod_ball > y_vin + radius:
             y_kod_ball = 0 - radius
         if y_kod_ball < 0 - radius:
-            y_kod_ball = 1080 + radius
-
-
-
-
+            y_kod_ball = 800 + radius
         if event.key == pygame.K_HOME:
-            farge_ball = (randint(1,255), randint(1,255), randint(1,255))
+            farge_ball = (randint(1, 255), randint(1, 255), randint(1, 255))
         if event.key == pygame.K_TAB:
             radius += R
         if event.key == pygame.K_DELETE:
