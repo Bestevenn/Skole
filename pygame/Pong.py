445a2ffe01bb2949b581f09fa7dcdd5d5368c3cc
mkn025 <78483320+mkn025@users.noreply.github.com>
2021-11-01 07:54:45 +0100

    pygame movemnet

--- pygame/Pong.py
+++ pygame/Pong.py
@@ -1,180 +1,55 @@
-import random
 import pygame
-import sys
-from pygame.locals import *
+from random import randint
+pygame.init
 
-pygame.init()
-fps = pygame.time.Clock()
+x_vin, y_vin = (1920),(1080)
+backgrunn = (30, 30, 30)
+farge_ball = (138, 180, 248)
+fps = 120 
+vindu = pygame.display.set_mode((x_vin, y_vin))
+clock = pygame.time.Clock()
 
-# colors
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-
-# globals
-WIDTH = 600
-HEIGHT = 400
-BALL_RADIUS = 20
-PAD_WIDTH = 8
-PAD_HEIGHT = 80
-HALF_PAD_WIDTH = PAD_WIDTH / 2
-HALF_PAD_HEIGHT = PAD_HEIGHT / 2
-ball_pos = [0, 0]
-ball_vel = [0, 0]
-paddle1_vel = 0
-paddle2_vel = 0
-l_score = 0
-r_score = 0
-
-# canvas declaration
-window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
-pygame.display.set_caption('Hello World')
-
-# helper function that spawns a ball, returns a position vector and a velocity vector
-# if right is True, spawn to the right, else spawn to the left
-
-
-def ball_init(right):
-    global ball_pos, ball_vel  # these are vectors stored as lists
-    ball_pos = [WIDTH/2, HEIGHT/2]
-    horz = random.randrange(2, 4)
-    vert = random.randrange(1, 3)
-
-    if right == False:
-        horz = - horz
-
-    ball_vel = [horz, -vert]
-
-# define event handlers
-
-
-def init():
-    # these are floats
-    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel, l_score, r_score
-    global score1, score2  # these are ints
-    paddle1_pos = [HALF_PAD_WIDTH - 1, HEIGHT/2]
-    paddle2_pos = [WIDTH + 1 - HALF_PAD_WIDTH, HEIGHT/2]
-    l_score = 0
-    r_score = 0
-    if random.randrange(0, 2) == 0:
-        ball_init(True)
-    else:
-        ball_init(False)
-
-
-# draw function of canvas
-def draw(canvas):
-    global paddle1_pos, paddle2_pos, ball_pos, ball_vel, l_score, r_score
-
-    canvas.fill(BLACK)
-    pygame.draw.line(canvas, WHITE, [WIDTH / 2, 0], [WIDTH / 2, HEIGHT], 1)
-    pygame.draw.line(canvas, WHITE, [PAD_WIDTH, 0], [PAD_WIDTH, HEIGHT], 1)
-    pygame.draw.line(
-        canvas, WHITE, [WIDTH - PAD_WIDTH, 0], [WIDTH - PAD_WIDTH, HEIGHT], 1)
-    pygame.draw.circle(canvas, WHITE, [WIDTH//2, HEIGHT//2], 70, 1)
-
-    # update paddle's vertical position, keep paddle on the screen
-    if paddle1_pos[1] > HALF_PAD_HEIGHT and paddle1_pos[1] < HEIGHT - HALF_PAD_HEIGHT:
-        paddle1_pos[1] += paddle1_vel
-    elif paddle1_pos[1] == HALF_PAD_HEIGHT and paddle1_vel > 0:
-        paddle1_pos[1] += paddle1_vel
-    elif paddle1_pos[1] == HEIGHT - HALF_PAD_HEIGHT and paddle1_vel < 0:
-        paddle1_pos[1] += paddle1_vel
-
-    if paddle2_pos[1] > HALF_PAD_HEIGHT and paddle2_pos[1] < HEIGHT - HALF_PAD_HEIGHT:
-        paddle2_pos[1] += paddle2_vel
-    elif paddle2_pos[1] == HALF_PAD_HEIGHT and paddle2_vel > 0:
-        paddle2_pos[1] += paddle2_vel
-    elif paddle2_pos[1] == HEIGHT - HALF_PAD_HEIGHT and paddle2_vel < 0:
-        paddle2_pos[1] += paddle2_vel
-
-    # update ball
-    ball_pos[0] += int(ball_vel[0])
-    ball_pos[1] += int(ball_vel[1])
-
-    # draw paddles and ball
-    pygame.draw.circle(canvas, RED, ball_pos, 20, 0)
-    pygame.draw.polygon(canvas, GREEN, [[paddle1_pos[0] - HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT], [paddle1_pos[0] - HALF_PAD_WIDTH, paddle1_pos[1] + HALF_PAD_HEIGHT], [
-                        paddle1_pos[0] + HALF_PAD_WIDTH, paddle1_pos[1] + HALF_PAD_HEIGHT], [paddle1_pos[0] + HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT]], 0)
-    pygame.draw.polygon(canvas, GREEN, [[paddle2_pos[0] - HALF_PAD_WIDTH, paddle2_pos[1] - HALF_PAD_HEIGHT], [paddle2_pos[0] - HALF_PAD_WIDTH, paddle2_pos[1] + HALF_PAD_HEIGHT], [
-                        paddle2_pos[0] + HALF_PAD_WIDTH, paddle2_pos[1] + HALF_PAD_HEIGHT], [paddle2_pos[0] + HALF_PAD_WIDTH, paddle2_pos[1] - HALF_PAD_HEIGHT]], 0)
-
-    # ball collision check on top and bottom walls
-    if int(ball_pos[1]) <= BALL_RADIUS:
-        ball_vel[1] = - ball_vel[1]
-    if int(ball_pos[1]) >= HEIGHT + 1 - BALL_RADIUS:
-        ball_vel[1] = -ball_vel[1]
-
-    # ball collison check on gutters or paddles
-    if int(ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH and int(ball_pos[1]) in range(paddle1_pos[1] - HALF_PAD_HEIGHT, paddle1_pos[1] + HALF_PAD_HEIGHT, 1):
-        ball_vel[0] = -ball_vel[0]
-        ball_vel[0] *= 1.1
-        ball_vel[1] *= 1.1
-    elif int(ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH:
-        r_score += 1
-        ball_init(True)
-
-    if int(ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH and int(ball_pos[1]) in range(paddle2_pos[1] - HALF_PAD_HEIGHT, paddle2_pos[1] + HALF_PAD_HEIGHT, 1):
-        ball_vel[0] = -ball_vel[0]
-        ball_vel[0] *= 1.1
-        ball_vel[1] *= 1.1
-    elif int(ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH:
-        l_score += 1
-        ball_init(False)
-
-    # update scores
-    myfont1 = pygame.font.SysFont("Comic Sans MS", 20)
-    label1 = myfont1.render("Score "+str(l_score), 1, (255, 255, 0))
-    canvas.blit(label1, (50, 20))
-
-    myfont2 = pygame.font.SysFont("Comic Sans MS", 20)
-    label2 = myfont2.render("Score "+str(r_score), 1, (255, 255, 0))
-    canvas.blit(label2, (470, 20))
-
-
-# keydown handler
-def keydown(event):
-    global paddle1_vel, paddle2_vel
-
-    if event.key == K_UP:
-        paddle2_vel = -8
-    elif event.key == K_DOWN:
-        paddle2_vel = 8
-    elif event.key == K_w:
-        paddle1_vel = -8
-    elif event.key == K_s:
-        paddle1_vel = 8
-
-# keyup handler
-
-
-def keyup(event):
-    global paddle1_vel, paddle2_vel
-
-    if event.key in (K_w, K_s):
-        paddle1_vel = 0
-    elif event.key in (K_UP, K_DOWN):
-        paddle2_vel = 0
-
-
-init()
-
-
-# game loop
+x_kod_ball = 960
+y_kod_ball = 540
+radius = 50 
+dx = 5
+dy = 5
+R = 2.5
 while True:
-
-    draw(window)
-
     for event in pygame.event.get():
-
-        if event.type == KEYDOWN:
-            keydown(event)
-        elif event.type == KEYUP:
-            keyup(event)
-        elif event.type == QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+    vindu.fill(backgrunn)
+    pygame.draw.circle(vindu, farge_ball, (x_kod_ball, y_kod_ball), radius, width=0)
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            x_kod_ball -= dx
+        if event.key == pygame.K_RIGHT:
+            x_kod_ball += dx
+        if event.key == pygame.K_UP:
+            y_kod_ball -= dy
+        if event.key == pygame.K_DOWN:
+            y_kod_ball += dy
+        if x_kod_ball > x_vin + radius:
+            x_kod_ball = 0 - radius
+        if x_kod_ball < 0 - radius:
+            x_kod_ball = 1920 + radius
+        
+        if y_kod_ball > y_vin + radius:
+            y_kod_ball = 0 - radius
+        if y_kod_ball < 0 - radius:
+            y_kod_ball = 1080 + radius
+
+
+
+
+        if event.key == pygame.K_HOME:
+            farge_ball = (randint(1,255), randint(1,255), randint(1,255))
+        if event.key == pygame.K_TAB:
+            radius += R
+        if event.key == pygame.K_DELETE:
+            radius -= R
+        if radius > x_vin/2 or radius < 0:
+            radius = 50
 
     pygame.display.update()
-    fps.tick(60)
