8bc35cb532f3700bfa35ab2286a108bf4d68c343
mkn025 <78483320+mkn025@users.noreply.github.com>
2021-11-10 19:26:51 +0100

    main

--- pygame/Sprettball.py
+++ pygame/Sprettball.py
@@ -1,4 +1,25 @@
 import pygame
 
-pygame.init
+pygame.init()
 
+
+hvit = (255, 255, 255)
+x_vin, y_vin = (800, 800)
+FPS = 200
+klokke = pygame.time.Clock()
+
+vindu = pygame.display.set_mode((x_vin, y_vin))
+
+
+def car(x, y):
+
+    Bilde = ("/Users/martinknutsen/Downloads/racecar.png")
+    vis_bilde = pygame.image.load(Bilde)
+    vindu.blit(vis_bilde, (x, y))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+
+            pygame.quit()
