0d91fc8ec39d25fdf1c4501c79f4bc5d5955c9c4
mkn025 <78483320+mkn025@users.noreply.github.com>
2021-11-10 19:26:27 +0100

    main

--- pygame/Køddespill.py
+++ pygame/Køddespill.py
@@ -9,6 +9,8 @@ fps = 120
 vindu = pygame.display.set_mode((x_vin, y_vin))
 clock = pygame.time.Clock()
 
+Bilde = pygame.image.load("/Users/martinknutsen/opt/anaconda3/racecar.png")
+
 x_kod_ball = 400
 y_kod_ball = 400
 x_kod_ball2 = 100
@@ -17,33 +19,29 @@ radius = 50
 radius_ball2 = 50
 dx = 5
 dy = 5
+dx_aks = 1.0
 R = 2.5
+grader = 0
+
+
+Bilde = pygame.transform.rotate(Bilde, grader)
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
     vindu.fill(backgrunn)
-    pygame.draw.circle(vindu, farge_ball, (x_kod_ball,
-                       y_kod_ball), radius, width=0)
-    pygame.draw.circle(vindu, farge_ball, (x_kod_ball2,
-                       y_kod_ball2), radius, width=0)
-    x_kod_ball2 += 1
-    y_kod_ball2 += 1
-    if x_kod_ball2 > x_vin + radius:
-        x_kod_ball2 -= dx
-    if x_kod_ball2 < x_vin - radius:
-        x_kod_ball2 += dx
-    if y_kod_ball2 > y_vin + radius:
-        y_kod_ball2 -= dy
-    if y_kod_ball2 < y_vin - radius:
-        y_kod_ball2 += dx
-    print(y_kod_ball2)
-    print(x_kod_ball2)
+
+    #pygame.draw.circle(vindu, farge_ball, (x_kod_ball, y_kod_ball), radius, width=0)
+    vindu.blit(Bilde, (x_kod_ball, y_kod_ball))
+
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_LEFT:
             x_kod_ball -= dx
         if event.key == pygame.K_RIGHT:
             x_kod_ball += dx
+            grader = 90
         if event.key == pygame.K_UP:
             y_kod_ball -= dy
         if event.key == pygame.K_DOWN:
@@ -58,13 +56,5 @@ while True:
             y_kod_ball = 0 - radius
         if y_kod_ball < 0 - radius:
             y_kod_ball = 800 + radius
-        if event.key == pygame.K_HOME:
-            farge_ball = (randint(1, 255), randint(1, 255), randint(1, 255))
-        if event.key == pygame.K_TAB:
-            radius += R
-        if event.key == pygame.K_DELETE:
-            radius -= R
-        if radius > x_vin/2 or radius < 0:
-            radius = 50
-
+    pygame.display.flip()
     pygame.display.update()
