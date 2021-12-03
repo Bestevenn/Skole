f08160987f98977d80737db8c8c0cc1811af58bd
mkn025 <78483320+mkn025@users.noreply.github.com>
2021-12-03 09:31:17 +0100

    main

--- pygame/Bilspill.py
+++ pygame/Bilspill.py
@@ -6,11 +6,6 @@ pygame.init
 pygame.font.init()
 
 
-print("skriv 1 på begge for mac")
-print("ikke ha med anførselstegn")
-bane_bilde = input("Skriv banen til bakgrunnen: ")
-bane_bil = input("Skriv banen til bil: ")
-
 pygame.display.set_caption("Bilspill")
 x_vin, y_vin = (1280), (720)
 backgrunn = (30, 30, 30)
@@ -20,32 +15,28 @@ vindu = pygame.display.set_mode((x_vin, y_vin))
 clock = pygame.time.Clock()
 green = (0, 255, 0)
 blue = (0, 0, 128)
+kant = 50
 
 
-if bane_bilde and bane_bil == "1":
-    Bilde = pygame.image.load("/Users/martinknutsen/opt/anaconda3/racecar.png")
-    bane = pygame.image.load("/Users/martinknutsen/opt/anaconda3/track2.png")
-else:
-    bane = pygame.image.load(f"{bane_bilde}")
-    Bilde = pygame.image.load(f"{bane_bil}")
+
+Bilde = pygame.image.load("/Users/martinknutsen/opt/anaconda3/racecar.png")
+bane = pygame.image.load("/Users/martinknutsen/opt/anaconda3/track3.jpeg")
+
 
 
 
 start_x = 110 
 start_y = 400
-
-x_kod_ball = start_x
-y_kod_ball = start_y
 radius = 50
 dx_aks = 1.0
 R = 2.5
 grader = 180
+x_kod_ball = start_x
+y_kod_ball = start_y
 fart = 0.001
 
 
 # teller og tekst
-
-
 poeng_verdi = 0
 font = pygame.font.SysFont('arial', 32)
 x_font = 600
@@ -62,7 +53,29 @@ def tegnfigur(vindu1, fig, punkt, vinkel):
     vindu1.blit(rotertbilde, rotertbilde.get_rect(
         center=fig.get_rect(center=(x, y)).center).topleft)
 
-kant = 50
+
+#checkpoints
+
+checkpoint_nr1,checkpoint_nr1_kod_x,checkpoint_nr1_kod_y = (234,125,69),(250),(100)
+checkpoint_nr2 = (125,70,234)
+checkpoint_nr3 = (70,179,235)
+checkpoint_nr4 = (125,70,234)
+status_checkpoint = 0
+
+
+# sarte på nytt
+def start_nytt(pros_x, pros_y,antall_grader):
+    global x_kod_ball
+    global y_kod_ball
+    global fart
+    global poeng_verdi
+    global grader
+    x_kod_ball = pros_x
+    y_kod_ball = pros_y
+    fart = 0.001
+    poeng_verdi = 0 
+    grader = antall_grader
+
 
 while True:
     for event in pygame.event.get():
@@ -75,10 +88,10 @@ while True:
     key = pygame.key.get_pressed()
     # Test for pil ned
     if key[pygame.K_LEFT]:
-        grader += 3
+        grader += 2
         # Test for pil opp
     elif key[pygame.K_RIGHT]:
-        grader -= 3
+        grader -= 2
     elif key[pygame.K_ESCAPE]:
         pygame.quit()
     elif key[pygame.K_UP]:
@@ -97,29 +110,37 @@ while True:
         y_kod_ball = y_vin - radius 
     if y_kod_ball < 0 - radius + kant:
         y_kod_ball = 0 - radius + kant
-
+    
     # fargegjennkjennig
-   
     
     farge_bil = bane.get_at((int(x_kod_ball), int(y_kod_ball)))
     
+    if farge_bil == checkpoint_nr1:
+        status_checkpoint = 1
+    elif farge_bil == checkpoint_nr2:
+        status_checkpoint = 2
 
-    if farge_bil == (255,38,0) or farge_bil == (50,124,11):
+    
+    print(status_checkpoint,"kordinater=", x_kod_ball, y_kod_ball, "grader =")
+    if farge_bil == (69,235,125) or farge_bil == (50,124,11):
         poeng_verdi += 1
         if poeng_verdi >= 1:
-            fart /= 2
-        if poeng_verdi == 5:
-            x_kod_ball = start_x
-            y_kod_ball = start_y
-            fart = 0.001
-            poeng_verdi = 0 
-        print("ani", "treff =", poeng_verdi)
+            fart /= 1.1112
+    
         
-    else:
-        print("ikke ani", "treff =", poeng_verdi)
+        if poeng_verdi == 100:
+            if status_checkpoint == 0:
+                start_nytt(start_x, start_y,180)        
+            elif status_checkpoint == 1:
+                start_nytt(checkpoint_nr1_kod_x, checkpoint_nr1_kod_y,90)
+            elif status_checkpoint == 2:
+                start_nytt(x_vin/2,y_vin/2,45)
+        
+     
 
     vis_poeng(x_font, y_font)
     tegnfigur(vindu, Bilde, (x_kod_ball, y_kod_ball), grader)
     pygame.display.flip()
     clock.tick(fps)
+
 
