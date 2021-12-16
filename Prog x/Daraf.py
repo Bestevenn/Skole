9dcc8f3c4a11fa1dd2f5c9d6b2eb6b3a3e2593d8
mkn025 <78483320+mkn025@users.noreply.github.com>
2021-12-16 10:10:47 +0100

    main

---
+++ Prog x/Daraf.py
@@ -0,0 +1,11 @@
+
+
+
+
+Grunnlinje = float(input("Hva er Grunnlinjen?: ")) # spør om grunnlinje
+Høyde = float(input("Hva er Høyden?: ")) # spør om høyde
+
+Areal_trekant = (Grunnlinje*Høyde)/(2) # regner ut Areal av trekant
+print("Arealet av trekanten med Høyde", Høyde,"og Grunnlinje",Grunnlinje,"=",Areal_trekant) # skriver ut Areal
+
+
